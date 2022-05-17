d0dcf16ce9a13868269776ddce3dbfe9a9346c8d
Nikola Markovic <53098565+nikola93kg@users.noreply.github.com>
2022-05-17 11:13:51 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, createContext } from 'react';
+import Board from './Components/Board';
+import Keyboard from './Components/Keyboard';
+import { boardDefault, generateWordSet } from './Words'
+import "./App.css";
+
+export const AppContext = createContext()
 
 function App() {
+
+  const [board, setBoard] = useState(boardDefault);
+  const [currentAttempt, setCurrentAttempt] = useState({ attempt: 0, letterPosition: 0 });
+  const [wordSet, setWordSet] = useState(new Set());
+  const [disabledLetters, setDisabledLetters] = useState([]);
+
+  const correctWord = 'RIGHT';
+
+  useEffect(() => {
+    generateWordSet()
+      .then(words => {
+        setWordSet(words.wordSet) //wordSet is an obj from words.js when we are returning it
+      })
+  }, []);
+
+  const onSelectLetter = (keyVal) => {
+    if (currentAttempt.letterPosition > 4) return;
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPosition] = keyVal;
+    setBoard(newBoard);
+    setCurrentAttempt({ ...currentAttempt, letterPosition: currentAttempt.letterPosition + 1 })
+  }
+  const onDelete = () => {
+    if (currentAttempt.letterPosition === 0) return;
+    const newBoard = [...board];
+    newBoard[currentAttempt.attempt][currentAttempt.letterPosition - 1] = '';
+    setBoard(newBoard)
+    setCurrentAttempt({ ...currentAttempt, letterPosition: currentAttempt.letterPosition - 1 })
+  }
+  const onEnter = () => {
+    if (currentAttempt.letterPosition !== 5) return;
+
+    let currentWord = '';
+    for (let i = 0; i < 5; i++) {
+      currentWord += board[currentAttempt.attempt][i];
+    }
+
+    //PROVERI OVAJ DEO KODA!!!
+
+    if (wordSet.has(currentWord.toLowerCase())) {
+      setCurrentAttempt({ attempt: currentAttempt.attempt + 1, letterPosition: 0 })
+    } else {
+      setCurrentAttempt({ attempt: currentAttempt.attempt + 1, letterPosition: 0 })
+    }
+
+    if (currentWord === correctWord) {
+      alert('game ended')
+    }
+
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <nav>
+        <h1>Wordle</h1>
+      </nav>
+      <AppContext.Provider value={{
+        board,
+        setBoard,
+        currentAttempt,
+        setCurrentAttempt,
+        onSelectLetter,
+        onDelete,
+        onEnter,
+        correctWord,
+        disabledLetters,
+        setDisabledLetters
+      }}>
+        <div className="game">
+          <Board />
+          <Keyboard />
+        </div>
+      </AppContext.Provider>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
